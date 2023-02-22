364b9857607ce1f6287bd047c2eea08d9f5c6a55
Ricardo <116693915+rickybbta@users.noreply.github.com>
2023-02-22 13:48:50 -0300

    Add files via upload

---
+++ Contexts/CiaAereaContext.cs
@@ -0,0 +1,35 @@
+using CiaAerea.Entities;
+using Microsoft.EntityFrameworkCore;
+using CiaAerea.EntityConfigurations;
+
+namespace CiaAerea.Contexts;
+
+public class CiaAereaContext: DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public CiaAereaContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
+    public DbSet<Piloto> Pilotos => Set<Piloto>();
+    public DbSet<Voo> Voos => Set<Voo>();
+    public DbSet<Cancelamento> Cancelamentos => Set<Cancelamento>();
+    public DbSet<Manutencao> Manutencoes => Set<Manutencao>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("CiaAerea"));
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AeronaveConfiguration());
+        modelBuilder.ApplyConfiguration(new PilotoConfiguration());
+        modelBuilder.ApplyConfiguration(new VooConfiguration());
+        modelBuilder.ApplyConfiguration(new CancelamentoConfiguration());
+        modelBuilder.ApplyConfiguration(new ManutencaoConfiguration());
+    }
+}
